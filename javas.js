001f162c9e0900b5df7259702fee9e1fd7a61472
JuanCruzDuran <juan.cruz.duran169@gmail.com>
2023-06-29 17:40:38 -0300

    la lista se guarda

--- javas.js
+++ javas.js
@@ -143,35 +143,125 @@ function borrar(){
   
 //lista to do
 
-function notaNueva(){
-  var contenedor = document.getElementById('listaToDo');
-  var contenido = document.createElement('p');
-  var content = document.getElementById('notas');
-  var nota = document.createElement('input');
-  var btnborrar = document.createElement('button');
+document.addEventListener('DOMContentLoaded', function() {
+  mostrarTareas();
 
-  if (content.value === "") {
-    return;
-  };
+  function notaNueva() {
+    var contenedor = document.getElementById('listaToDo');
+    var contenido = document.createElement('p');
+    var content = document.getElementById('notas');
+    var nota = document.createElement('input');
+    var btnborrar = document.createElement('button');
 
-  nota.type = 'checkbox';
+    if (content.value === "") {
+      return;
+    }
 
-  contenido.textContent = content.value;
-  contenedor.appendChild(nota);
-  contenedor.appendChild(contenido);
-  contenedor.appendChild(btnborrar);
-  
-  btnborrar.onclick = function () {
-    contenedor.removeChild(nota);
-    contenedor.removeChild(contenido);
-    contenedor.removeChild(btnborrar);
-  };
-  btnborrar.textContent = 'borrar';
+    nota.type = 'checkbox';
 
-  content.value = '';
-}
+    contenido.textContent = content.value;
+    contenedor.appendChild(nota);
+    contenedor.appendChild(contenido);
+    contenedor.appendChild(btnborrar);
 
+    var nuevaTarea = {
+      contenido: content.value,
+      completada: false
+    };
 
+    var tareas = obtenerTareas();
+    tareas.push(nuevaTarea);
+    guardarTareas(tareas);
+
+    btnborrar.onclick = function () {
+      contenedor.removeChild(nota);
+      contenedor.removeChild(contenido);
+      contenedor.removeChild(btnborrar);
+
+      var tareas = obtenerTareas();
+      var index = tareas.findIndex(function (tarea) {
+        return tarea.contenido === nuevaTarea.contenido;
+      });
+      if (index !== -1) {
+        tareas.splice(index, 1);
+      }
+      guardarTareas(tareas);
+    };
+    btnborrar.textContent = 'borrar';
+
+    content.value = '';
+  }
+
+  function obtenerTareas() {
+    const tareas = localStorage.getItem('tareas');
+    if (tareas) {
+      return JSON.parse(tareas);
+    } else {
+      return [];
+    }
+  }
+
+  function guardarTareas(tareas) {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }
+
+  function mostrarTareas() {
+    var contenedor = document.getElementById('listaToDo');
+    contenedor.innerHTML = '';
+
+    var tareas = obtenerTareas();
+
+    tareas.forEach(function (tarea) {
+      var nota = document.createElement('input');
+      var contenido = document.createElement('p');
+      var btnborrar = document.createElement('button');
+
+      nota.type = 'checkbox';
+      contenido.textContent = tarea.contenido;
+
+      nota.checked = tarea.completada;
+
+      contenedor.appendChild(nota);
+      contenedor.appendChild(contenido);
+      contenedor.appendChild(btnborrar);
+
+      nota.onchange = function () {
+        tarea.completada = nota.checked;
+        var tareas = obtenerTareas();
+        var index = tareas.findIndex(function (t) {
+          return t.contenido === tarea.contenido;
+        });
+        if (index !== -1) {
+          tareas[index].completada = tarea.completada;
+        }
+        guardarTareas(tareas);
+      };
+
+      btnborrar.onclick = function () {
+        contenedor.removeChild(nota);
+        contenedor.removeChild(contenido);
+        contenedor.removeChild(btnborrar);
+
+        var tareas = obtenerTareas();
+        var index = tareas.findIndex(function (t) {
+          return t.contenido === tarea.contenido;
+        });
+        if (index !== -1) {
+          tareas.splice(index, 1);
+        }
+        guardarTareas(tareas);
+      };
+
+      btnborrar.textContent = 'borrar';
+    });
+  }
+
+  var form = document.getElementById('formulario');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    notaNueva();
+  });
+});
 
 //to do list GPT
 function addTask() {
@@ -322,4 +412,33 @@ function randomImg() {
   var srcActual = imagen.src;
   var timestamp = new Date().getTime(); // Genera un timestamp único
   imagen.src = srcActual + "?timestamp=" + timestamp;
+}
+
+var imagenes = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIqIlGHgf1YJfPEZ7saB8WSOYj5nqekiCvqA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkViOnhOO30R_qjaBKzxQkRttOhq7o6PlatA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-E7PuX_JBVFmpwRiOXGx_zN87iPBdguRsWQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo8_u8XwRFCpu6W8DmiAuINZutk8W-Z7U26A&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL2CU9wZZwg7zNA9QWNGYmJod1rrKrGcb4cQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv3T-IcWtOmM3S2fkZp2FOEKQ3oMADur2hKg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCu4PjG5h7FN1AtWlUlL_d9CY7NepVY0yIZw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_q6LQhSCHu2Mmo7Ol3gAyToy0zfIG7OdFog&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYgz6kOFmWqaAXXseQvLGpu0oSMDNehLNwkQ&usqp=CAU"
+];
+
+var indiceActual = 0; // Variable para rastrear el índice actual
+
+function pasarImg(param) {
+  var img = document.getElementById("galeria1");
+  
+  // Actualizar el índice en función del parámetro recibido
+  indiceActual = (indiceActual + param) % imagenes.length;
+  
+  // Ajustar el índice si es menor que cero
+  if (indiceActual < 0) {
+    indiceActual = imagenes.length - 1;
+  }
+
+  // Establecer la imagen actual
+  img.src = imagenes[indiceActual];
 }
